28790cfa54f6f9cfe04791ee9bab62ce4784a28d
Artem Demidov <artem.demidov88@gmail.com>
2017-09-02 18:28:13 +0300

    Changed get result rates

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var NbrbAPI = require('./lib/nbrbapi');
 
 var api = new NbrbAPI(null, "http");
 
-
-api.setParameters({'curID':'298'});
+api.setParameters({'curID':'298', 'onDate':'07-05-2017'});
+api.request('rates', function (error, result) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+    }
+});
