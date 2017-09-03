d4095c238a555052f971c9cf987896d61231597d
Artem Demidov <artem.demidov88@gmail.com>
2017-09-03 11:03:26 +0300

    Fixed dynamic rates

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var NbrbAPI = require('./lib/nbrbapi');
 
 var api = new NbrbAPI(null, "http");
 
-api.setParameters({'curID':'298', 'onDate':'07-05-2017'});
-api.request('rates', function (error, result) {
+api.setParameters({'curID':'298', 'startDate': '2017-6-1','endDate': '2017-7-30'});
+//api.setParameters({'periodicity':0});
+//api.setParameters({'curName': 'USD'});
+api.request('dynamicrates', function (error, result) {
     if (error) {
         console.log(error);
     } else {
